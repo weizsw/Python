d5d384a8e709c716e9b49f1c179af09dcba45276
weizsw <weizsw888@gmail.com>
2019-06-29 16:39:39 -0700

    emmm

---
+++ one.py
@@ -0,0 +1,12 @@
+#one.py
+
+def func():
+    print('fun() in one.py')
+
+print('top level in one.py')
+
+
+if __name__ == '__main__':
+    print('one.py is being run directly!')
+else:
+    print('one.py has been imported!')
